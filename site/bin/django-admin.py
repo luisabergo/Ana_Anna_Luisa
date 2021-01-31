30405cdfb9f33aaa4dfc843684c23a3bd5c0cf18
luisaribeiro13 <luisasilvaribeiro@yahoo.com.br>
2021-01-31 12:30:26 -0300

    Configuração ambiente

---
+++ site/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/luisa/Documentos/UFJF/Eng de Software/Engenharia de Software/site/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
