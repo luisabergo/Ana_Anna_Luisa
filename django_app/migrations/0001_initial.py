1114d46c74492f8d6043b46e0892961789f6edac
Anna Luisa <you@example.com>
2021-03-10 10:17:03 -0300

    banco de dados corrigido

--- django_app/migrations/0001_initial.py
+++ django_app/migrations/0001_initial.py
@@ -14,8 +14,8 @@ class Migration(migrations.Migration):
             name='Aluno',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
-                ('nome', models.CharField(max_length=100, db_index=True)),
-                ('pontuacao', models.IntegerField()),
+                ('pontuacao', models.FloatField()),
+                ('matricula', models.CharField(max_length=100, db_index=True)),
             ],
         ),
         migrations.CreateModel(
@@ -50,7 +50,6 @@ class Migration(migrations.Migration):
             name='Professor',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
-
                 ('phone', models.CharField(max_length=15)),
                 ('siape', models.CharField(max_length=100, db_index=True)),
             ],
